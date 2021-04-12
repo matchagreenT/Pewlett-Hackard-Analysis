6fb6f92b06993a0c976bf8820008acb4d4f32985
matchagreenT <77694480+matchagreenT@users.noreply.github.com>
2021-04-12 18:03:06 -0400

    Add files via upload

--- Queries/Schema.sql
+++ Queries/Schema.sql
@@ -17,7 +17,7 @@ CREATE TABLE employees (
 );
 
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
@@ -33,6 +33,22 @@ CREATE TABLE salaries (
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE dept_emp (
+	dept_no VARCHAR NOT NULL,
+	emp_no INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	PRIMARY KEY (dept_no)
+);
+
+CREATE TABLE titles (
+	emp_no VARCHAR NOT NULL,
+	title INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	PRIMARY KEY (emp_no)
 );
 
 SELECT * FROM departments;
