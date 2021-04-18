5aa6b91bbb0f67596243103fb526be6365f418bf
matchagreenT <77694480+matchagreenT@users.noreply.github.com>
2021-04-17 21:41:54 -0400

    Add files via upload

--- Queries/Employee_DB_Challenge.sql
+++ Queries/Employee_DB_Challenge.sql
@@ -18,16 +18,33 @@ CREATE TABLE titles (
  	PRIMARY KEY (emp_no, title, from_date)
 );
 
-drop table retirement_line cascade
+CREATE TABLE dept_emp (
+ 	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL,
+ 	to_date DATE NOT NULL,
+	PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE departments (
+     dept_no VARCHAR(4) NOT NULL,
+     dept_name VARCHAR(40) NOT NULL,
+     PRIMARY KEY (dept_no),
+     UNIQUE (dept_name)
+);
+
+drop table unique_titles cascade
 
 -- Creating retirement tables for retirees
 SELECT e.emp_no,
 	e.first_name,
 	e.last_name,
+	e.birth_date,
+	e.hire_date,
     t.title,
     t.from_date,
 	t.to_date
-INTO retirement_line
+INTO unique_titles
 FROM employees as e
 INNER JOIN titles as t
 ON (e.emp_no = t.emp_no)
@@ -46,6 +63,27 @@ title
 INTO retires
 FROM retirement_line
 
-drop table retires cascade
 
-select * from retires
+select * from unique_titles
+
+-- Create Mentorship table
+SELECT e.emp_no,
+	   e.first_name,
+	   e.last_name,
+	   e.birth_date,
+	   de.from_date,
+	   de.to_date,
+	   t.title
+INTO mentorship_eligibility
+FROM unique_titles as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	 AND (de.to_date = '9999-01-01');
+
+select * from mentorship_eligibility
+
+drop table mentorship_eligibility
